1426a490e1a92cf5766515dea854dc393fe421da
wliub <wliub@DESKTOP-HTSJ8RC>
2019-02-17 15:12:48 +0800

    增加 初始化脚本 initialize.sh &&　修改日志目录

---
+++ initialize.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+# Filename:    initialize.sh 
+# Version:     2.0.0
+# Date:        2018-10-17
+# Author:      LiuBaoWen
+# Email:       bwliush@cn.ibm.com
+# Description: 初始化项目
+# Notes:       
+# -------------------------------------------------------------------------------
+
+#工作空间
+workspaces=$(dirname "$0")
+
+. ${workspaces}/common-constants
+
+#创建日志目录
+mkdir -p $log_dir
